d6e6936005dbaeedf9040f40d21c85f753ab8957
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-06-20 23:50:32 +0300

    moved Error Message and Loader files to components

---
+++ components/ErrorMesage/ErrorMesage.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+export const ErrorMessage = ({ message }: ErrorMessageProps) => {
+  useEffect(() => {
+    toast.error(message);
+  }, [message]);
+
+  return null;
+};
