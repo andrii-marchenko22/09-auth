d4ae9b8634b09e3af918a4202a825198c4aeadd5
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-06-21 00:03:13 +0300

    NoteList file moved

---
+++ components/NoteList/NoteList.tsx
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "@/lib/api";
+import css from "./NoteList.module.css";
+import type { Note } from "../../types/note";
+
+interface noteListProps {
+  notes: Note[];
+}
+
+export const NoteList = ({ notes }: noteListProps) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
+  return (
+    <ul className={css.list}>
+      {notes.map((note) => (
+        <li className={css.listItem} key={note.id}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button
+              className={css.button}
+              onClick={() => mutation.mutate(note.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
