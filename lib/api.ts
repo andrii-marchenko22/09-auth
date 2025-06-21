9bb5588ddddaf65a39b9d8d20f0b80418f86c631
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-06-21 23:01:37 +0300

    fixed api file

--- lib/api.ts
+++ lib/api.ts
@@ -4,20 +4,20 @@ import type { Note, NewPostCreate } from "../types/note";
 const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 const BASE_URL = "https://notehub-public.goit.study/api/notes";
 
-interface notesResponse {
+interface NotesResponse {
   notes: Note[];
   page: number;
   totalPages: number;
 }
 
-type fetchNotesResponse = notesResponse & { totalCount: number };
+type fetchNotesResponse = NotesResponse & { totalCount: number };
 
 export const fetchNotes = async (
   searchText: string,
   page = 1,
   perPage = 12
 ): Promise<fetchNotesResponse> => {
-  const response = await axios.get<notesResponse>(BASE_URL, {
+  const response = await axios.get<NotesResponse>(BASE_URL, {
     params: {
       ...(searchText !== "" && { search: searchText }),
       page,
@@ -33,7 +33,7 @@ export const fetchNotes = async (
   return { ...response.data, totalCount };
 };
 
-export const createNote = async (noteData: NewPostCreate) => {
+export const createNote = async (noteData: NewPostCreate): Promise<Note> => {
   const response = await axios.post<Note>(BASE_URL, noteData, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -42,7 +42,7 @@ export const createNote = async (noteData: NewPostCreate) => {
   return response.data;
 };
 
-export const deleteNote = async (notesId: number) => {
+export const deleteNote = async (notesId: number): Promise<Note> => {
   const response = await axios.delete<Note>(`${BASE_URL}/${notesId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -51,7 +51,7 @@ export const deleteNote = async (notesId: number) => {
   return response.data;
 };
 
-export const fetchNoteById = async (id: string) => {
+export const fetchNoteById = async (id: string): Promise<Note> => {
   const response = await axios.get<Note>(`${BASE_URL}/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
