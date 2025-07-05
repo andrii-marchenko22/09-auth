ab91946e0dd7e9932491b9369f1c2a550556cf11
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-07-06 01:01:00 +0300

    adeed metaData in layout and not-found files

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,23 @@
 import css from "./NotFound.module.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not found",
+  description: "Page-not-found",
+  openGraph: {
+    title: "Not-found",
+    description: "Page-not-found",
+    url: "https://07-routing-nextjs-coral.vercel.app/not-found",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Page Not Found - NoteHub",
+      },
+    ],
+  },
+};
 
 const NotFound = () => {
   return (
